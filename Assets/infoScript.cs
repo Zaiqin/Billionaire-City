d738a7ac5b138a1b4eba3fdcac483d6d4af5f351
Zaiqin <phuazaiqin@gmail.com>
2021-12-26 17:25:10 +0800

    Fixed infoPanel contract time bug
    Fix bug where properties created from new game had invalid signTime

--- Assets/infoScript.cs
+++ Assets/infoScript.cs
@@ -14,23 +14,32 @@ public class infoScript : MonoBehaviour
         nameText.GetComponent<Text>().text = selProp.GetComponent<Property>().Card.displayName;
         if (selProp.GetComponent<Property>().Card.type == "House")
         {
-            var diff = DateTime.Parse(selProp.transform.GetChild(0).gameObject.GetComponent<contractScript>().signTime) - System.DateTime.Now;
-            if (diff > TimeSpan.Zero)
+            if (selProp.transform.GetChild(0).gameObject.GetComponent<contractScript>().signTime != "notsigned")
             {
-                if (diff.Days != 0)
+                var diff = DateTime.Parse(selProp.transform.GetChild(0).gameObject.GetComponent<contractScript>().signTime) - System.DateTime.Now;
+                if (diff > TimeSpan.Zero)
                 {
-                    timeText.GetComponent<Text>().text = string.Format("{0:D2} Days {1:D2} hrs", diff.Days, diff.Hours);
-                } else if (diff.Hours != 0)
-                {
-                    timeText.GetComponent<Text>().text = string.Format("{0:D2} Hours {1:D2} mins", diff.Hours, diff.Minutes);
-                } else if (diff.Minutes != 0)
-                {
-                    timeText.GetComponent<Text>().text = string.Format("{0:D2} mins {1:D2} secs", diff.Minutes, diff.Seconds);
-                } else if (diff.Seconds != 0)
-                {
-                    timeText.GetComponent<Text>().text = string.Format("{0:D2} seconds", diff.Seconds);
+                    if (diff.Days != 0)
+                    {
+                        timeText.GetComponent<Text>().text = string.Format("{0:D2} Days {1:D2} hrs", diff.Days, diff.Hours);
+                    }
+                    else if (diff.Hours != 0)
+                    {
+                        timeText.GetComponent<Text>().text = string.Format("{0:D2} Hours {1:D2} mins", diff.Hours, diff.Minutes);
+                    }
+                    else if (diff.Minutes != 0)
+                    {
+                        timeText.GetComponent<Text>().text = string.Format("{0:D2} mins {1:D2} secs", diff.Minutes, diff.Seconds);
+                    }
+                    else if (diff.Seconds != 0)
+                    {
+                        timeText.GetComponent<Text>().text = string.Format("{0:D2} seconds", diff.Seconds);
+                    }
                 }
-                
+                else
+                {
+                    timeText.GetComponent<Text>().text = "No Contract Signed";
+                }
             }
             else
             {
