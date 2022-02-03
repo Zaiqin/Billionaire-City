57d25e3ba7bc17921735824082ad5a805a539a64
Zaiqin <phuazaiqin@gmail.com>
2022-02-03 15:16:04 +0800

    Added correct expansion costs and some world tiles

--- Assets/Scripts/expScript.cs
+++ Assets/Scripts/expScript.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class expScript : MonoBehaviour, IPointerClickHandler
 {
-
     public GameObject expPopup, cam, extAudio, text;
     public AudioClip touchsound;
     // Start is called before the first frame update
@@ -29,27 +28,37 @@ public class expScript : MonoBehaviour, IPointerClickHandler
             extAudio.GetComponent<AudioSource>().PlayOneShot(touchsound);
             expPopup.SetActive(true);
 
-            string cost = "";
-            if (int.Parse(this.name.Substring(this.name.Length - 2)) < 20)
+            int cost = 0;
+            switch (expPopup.GetComponent<expansion>().deletedExp.Count)
             {
-                cost = "$1,000,000";
-            } else if (int.Parse(this.name.Substring(this.name.Length - 2)) > 20 && int.Parse(this.name.Substring(this.name.Length - 2)) < 30)
-            {
-                cost = "$5,000,000";
+                case 0: cost = 4000000; break;
+                case 1: cost = 6000000; break;
+                case 2: cost = 7000000; break;
+                case 3: cost = 9000000; break;
+                case 4: cost = 30000000; break;
+                case 5: cost = 36000000; break;
+                case 6: cost = 42000000; break;
+                case 7: cost = 48000000; break;
+                case 8: cost = 60000000; break;
+                case 9: cost = 72000000; break;
+                case 10: cost = 84000000; break;
+                case 11: cost = 96000000; break;
+                case 12: cost = 180000000; break;
+                case 13: cost = 195000000; break;
+                case 14: cost = 210000000; break;
+                case 15: cost = 228000000; break;
+                case 16: cost = 300000000; break;
+                case 17: cost = 360000000; break;
+                case 18: cost = 420000000; break;
+                case 19: cost = 480000000; break;
+                case 20: cost = 540000000; break;
+                case 21: cost = 600000000; break;
+                case 22: cost = 660000000; break;
+                case 23: cost = 800000000; break;
+                default:
+                    break;
             }
-            else if (int.Parse(this.name.Substring(this.name.Length - 2)) > 30 && int.Parse(this.name.Substring(this.name.Length - 2)) < 40)
-            {
-                cost = "$10M";
-            }
-            else if (int.Parse(this.name.Substring(this.name.Length - 2)) > 40 && int.Parse(this.name.Substring(this.name.Length - 2)) < 50)
-            {
-                cost = "$50M";
-            }
-            else if (int.Parse(this.name.Substring(this.name.Length - 2)) > 50 && int.Parse(this.name.Substring(this.name.Length - 2)) < 60)
-            {
-                cost = "$100M";
-            }
-            text.GetComponent<Text>().text = cost;
+            text.GetComponent<Text>().text = "$" + cost.ToString("#,##0");
         }
     }
 }
--- Assets/Scripts/expansion.cs
+++ Assets/Scripts/expansion.cs
@@ -32,11 +32,10 @@ public class expansion : MonoBehaviour
         bool result = true;
         if (i > 10 && i < 20)
         {
-            cost = 1000000;
+            //cost = 1000000;
         }
         if (i > 20 && i < 30)
         {
-            cost = 5000000;
             foreach (int s in expInts)
             {
                 if (s > 10 && s < 20 ) { result = false; }
@@ -44,7 +43,6 @@ public class expansion : MonoBehaviour
         }
         else if (i > 30 && i < 40)
         {
-            cost = 10000000;
             foreach (int s in expInts)
             {
                 if (s > 20 && s < 30) { result = false; }
@@ -52,7 +50,6 @@ public class expansion : MonoBehaviour
         }
         else if (i > 40 && i < 50)
         {
-            cost = 50000000;
             foreach (int s in expInts)
             {
                 if (s > 30 && s < 40) { result = false; }
@@ -60,11 +57,39 @@ public class expansion : MonoBehaviour
         }
         else if (i > 50 && i < 60)
         {
-            cost = 100000000;
             foreach (int s in expInts)
             {
                 if (s > 40 && s < 50) { result = false; }
             }
+        }
+        switch (deletedExp.Count)
+        {
+            case 0: cost = 4000000; break;
+            case 1: cost = 6000000; break;
+            case 2: cost = 7000000; break;
+            case 3: cost = 9000000; break;
+            case 4: cost = 30000000; break;
+            case 5: cost = 36000000; break;
+            case 6: cost = 42000000; break;
+            case 7: cost = 48000000; break;
+            case 8: cost = 60000000; break;
+            case 9: cost = 72000000; break;
+            case 10: cost = 84000000; break;
+            case 11: cost = 96000000; break;
+            case 12: cost = 180000000; break;
+            case 13: cost = 195000000; break;
+            case 14: cost = 210000000; break;
+            case 15: cost = 228000000; break;
+            case 16: cost = 300000000; break;
+            case 17: cost = 360000000; break;
+            case 18: cost = 420000000; break;
+            case 19: cost = 480000000; break;
+            case 20: cost = 540000000; break;
+            case 21: cost = 600000000; break;
+            case 22: cost = 660000000; break;
+            case 23: cost = 800000000; break;
+            default:
+                break;
         }
         if (result == true && stats.GetComponent<Statistics>().returnStats()[0] >= cost)
         {
@@ -104,7 +129,10 @@ public class expansion : MonoBehaviour
             xOrig = x;
             for (int i = 0; i < spaceX * spaceY; i++)
             {
-                map.SetTile(new Vector3Int(x, y, 0), grassTile);
+                if (map.GetTile(new Vector3Int(x, y, 0)).name == "noBelow")
+                {
+                    map.SetTile(new Vector3Int(x, y, 0), grassTile);
+                }
                 x += 1;
                 if (x == (xOrig + spaceX))
                 {
