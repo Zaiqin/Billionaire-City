e8d0a05b9348b81eba669f97860a8abc4c7eb661
Zaiqin <phuazaiqin@gmail.com>
2022-02-13 15:07:57 +0800

    Tweaked floatingValue

--- Assets/Scripts/floatingValueHandler.cs
+++ Assets/Scripts/floatingValueHandler.cs
@@ -7,7 +7,7 @@ public class floatingValueHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 0.8f);
+        Destroy(gameObject, 0.6f);
     }
 
     // Update is called once per frame
